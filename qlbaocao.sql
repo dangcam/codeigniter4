dcc47ea3371816ce80532971eea1b98168a99e9e
Dang Cam <dangcam.pr@outlook.com>
2023-04-27 16:57:33 +0700

    report

--- qlbaocao.sql
+++ qlbaocao.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th4 17, 2023 lúc 11:30 AM
+-- Thời gian đã tạo: Th4 27, 2023 lúc 11:56 AM
 -- Phiên bản máy phục vụ: 10.4.27-MariaDB
 -- Phiên bản PHP: 8.2.0
 
@@ -76,6 +76,31 @@ INSERT INTO `groups` (`group_id`, `group_name`, `group_parent`, `group_status`) 
 -- --------------------------------------------------------
 
 --
+-- Cấu trúc bảng cho bảng `report_group`
+--
+
+CREATE TABLE `report_group` (
+  `report_month` int(11) NOT NULL,
+  `report_year` int(11) NOT NULL,
+  `group_id` varchar(20) NOT NULL,
+  `name_row` varchar(50) NOT NULL,
+  `value1_1` int(11) NOT NULL,
+  `value1_2` int(11) NOT NULL,
+  `value1_3` int(11) NOT NULL,
+  `value1_total` int(11) NOT NULL,
+  `value2_total` int(11) NOT NULL,
+  `value2_1` int(11) NOT NULL,
+  `value2_2` int(11) NOT NULL,
+  `value2_per` float NOT NULL,
+  `value3_total` int(11) NOT NULL,
+  `value3_1` int(11) NOT NULL,
+  `value3_2` int(11) NOT NULL,
+  `value_per` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Cấu trúc bảng cho bảng `users`
 --
 
@@ -95,9 +120,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `password`, `gender`, `email`, `phonenumber`, `group_id`, `user_status`) VALUES
-('admin', 'Đăng Cẩm', '8aee4a3c7c6d3b9bc83dedf0b42bdff0edac7ccd', 1, 'dangcam.pr@gmail.com', '0979371093', 'vpddt', 1),
-('admin1', 'Admin1', '8b2a31ad260da1ddd9e026d88d72dbfe42276f72', 1, 'admin1@gmail.com', '0979371093', 'vpddpr', 1),
-('admin2', 'Admin2', '2e7c8260125e7cdc02300c9ee56be000fad6ab52', 2, 'dangcam.pr@gmail.com', '0979371093', 'vpdddx', 1);
+('admin', 'Nguyễn Đăng Cẩm', 'b16f278e79dade5ef8e2207cf852d2e653e6c084', 1, 'dangcam.pr@outlook.com', '0979371093', 'vpddt', 1),
+('admin1', 'Admin 1', '8b2a31ad260da1ddd9e026d88d72dbfe42276f72', 1, 'admin1@gmail.com', '0979371093', 'vpddpr', 1),
+('admin2', 'Admin 2', '2e7c8260125e7cdc02300c9ee56be000fad6ab52', 2, 'dangcam.pr@gmail.com', '0979371093', 'vpdddx', 1);
 
 -- --------------------------------------------------------
 
@@ -115,6 +140,21 @@ CREATE TABLE `user_function` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
+-- Đang đổ dữ liệu cho bảng `user_function`
+--
+
+INSERT INTO `user_function` (`user_id`, `function_id`, `function_view`, `function_add`, `function_edit`, `function_delete`) VALUES
+('admin', 'function', 1, 1, 1, 1),
+('admin', 'group', 1, 1, 1, 1),
+('admin', 'user', 1, 1, 1, 1),
+('admin1', 'function', 0, 0, 1, 0),
+('admin1', 'group', 0, 0, 1, 0),
+('admin1', 'user', 0, 0, 1, 0),
+('admin2', 'function', 1, 0, 0, 0),
+('admin2', 'group', 0, 1, 0, 0),
+('admin2', 'user', 0, 1, 1, 0);
+
+--
 -- Chỉ mục cho các bảng đã đổ
 --
 
@@ -129,6 +169,12 @@ ALTER TABLE `functions`
 --
 ALTER TABLE `groups`
   ADD PRIMARY KEY (`group_id`);
+
+--
+-- Chỉ mục cho bảng `report_group`
+--
+ALTER TABLE `report_group`
+  ADD PRIMARY KEY (`report_month`,`report_year`,`group_id`,`name_row`);
 
 --
 -- Chỉ mục cho bảng `users`
