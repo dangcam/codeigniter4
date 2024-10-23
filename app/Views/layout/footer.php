59cf0617944dff5f99e4e797a222583157844aa3
DangCam <dangcam.pr@outlook.com>
2024-10-23 16:04:26 +0700

    tạm thời xong báo cáo phòng ban

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -5,7 +5,7 @@
 ***********************************-->
 <div class="footer">
     <div class="copyright">
-        <p>Copyright © Designed &amp; Developed by <a href="#" target="_blank">TieuMoStore</a> 2019</p>
+        <p>Copyright © Designed &amp; Phát Triển bởi <a href="#" target="_blank">VPĐKĐĐ</a> 2023</p>
     </div>
 </div>
 <!--**********************************
@@ -30,7 +30,17 @@
     Scripts
 ***********************************-->
 
-
+<script src="https://sf-cdn.coze.com/obj/unpkg-va/flow-platform/chat-app-sdk/0.1.0-beta.5/libs/oversea/index.js"></script>
+      <script>
+          new CozeWebSDK.WebChatClient({
+            config: {
+              bot_id: '7408473083272495111',
+            },
+            componentProps: {
+              title: 'Coze',
+            },
+          });
+      </script>
 
 </body>
 
